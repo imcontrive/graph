677aac2e1c0bf80d56e8f0f6ffe44646da4bbde8
shubham-torum <shubham@torum.com>
2024-05-12 18:14:48 +0530

    new commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginButton from './components/LoginButton';
+
+function App() {
+  return (
+    <div className="App">
+     <LoginButton />
+    </div>
+  );
+}
+
+export default App;
