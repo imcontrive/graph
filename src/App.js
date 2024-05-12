4260766574b051c4081b3f1731c941d295ac179c
shubham-torum <shubham@torum.com>
2024-05-12 21:03:49 +0530

    added new issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+// import GoogleLoginButtonView from './components/GoogleLoginButtonView';
 import LoginButton from './components/LoginButton';
+import { GoogleLogin } from '@react-oauth/google';
 
 function App() {
+  const responseMessage = (response) => {
+    console.log(response);
+};
+const errorMessage = (error) => {
+    console.log(error);
+};
   return (
     <div className="App">
       <h3>Graph demo</h3>
      <LoginButton />
+     {/* <GoogleLoginButtonView /> */}
+     <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
     </div>
   );
 }
---
+++ src/components/GoogleLoginButtonView.js
@@ -0,0 +1,29 @@
+// import React from 'react';
+// import GoogleLoginButton from 'react-google-login-button';
+
+// const GoogleLoginButtonView = ({ onSuccess, onFailure }) => {
+//   const CLIENT_ID = '614091881067-af2hdg61ivqbqlunt3un2eerhshfvkm2.apps.googleusercontent.com'; // Replace with your actual Client ID
+
+//   const handleSuccess = (googleUser) => {
+//     const idToken = googleUser.getAuthResponse().id_token;
+//     onSuccess(idToken);
+//   };
+
+//   const handleFailure = (error) => {
+   
+//     onFailure(error);
+//   };
+
+//   return (
+//     <GoogleLoginButton
+//       clientConfig={{
+//         client_id: '614091881067-af2hdg61ivqbqlunt3un2eerhshfvkm2.apps.googleusercontent.com',//'614091881067-af2hdg61ivqbqlunt3un2eerhshfvkm2.apps.googleusercontent.com',
+//         // scope: 'profile email',
+//       }}
+//       onSuccess={handleSuccess}
+//       onFailure={handleFailure}
+//     />
+//   );
+// };
+
+// export default GoogleLoginButtonView;
